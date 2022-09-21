1a3388a0d09e703cf82df3f7fec44fc94d2392a0
onesmus <onesmus1024@gmail.com>
2022-09-21 11:25:11 +0300

    update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import model
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import tensorflow as tf
 
 model.get_data()
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
